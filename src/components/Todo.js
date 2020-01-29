87b6013161a9defd21a583ab5b30fd267db3480b
unknown <seglifabian@gmail.com>
2020-01-28 18:58:04 -0600

    Todo App Add Task butoon added

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,17 +1,52 @@
-import React, {Components} from 'react'
+// import React, {Components} from 'react'
+
+// class Todo extends Component {
+// state = {
+//     text: 'initial'
+// }
+//     render() {
+//     return (
+//     <div>
+//         <input type='text'/>
+//         <button> Add Task</button>
+//     </div>
+//     )
+// }
+// }
+
+// export default Todo 
+import React, { Component } from 'react'
+
 
 class Todo extends Component {
-state = {
-    text: 'initial'
-}
+    state = {
+        text: '', 
+        listItems: []
+    }
+
+    handleTextChange = (e) => {
+        this.setState({ text: e.target.value})
+    }
+
+    handleClicked = () => {
+        const items = [ ...this.state.listItems ]
+        items.push(this.state.text)
+        this.setState({ listItems: items })
+    }
+
     render() {
-    return (
-    <div>
-        <input type='text'/>
-        <button> Add Task</button>
-    </div>
-    )
-}
+        return (
+           <div style={{ marginTop: '30px' }}>
+                <input type="text" onChange={this.handleTextChange} />
+                <button style={{ backgroundColor: 'gray', boxShadow: '5px 5px 5px black' }} onClick={this.handleClicked}>Add Task</button>
+                {
+                 this.state.listItems.map(value => {
+                    return <p>{ value }</p>
+               })
+             }
+           </div>
+        )
+    }
 }
 
-export default Todo 
+export default Todo
